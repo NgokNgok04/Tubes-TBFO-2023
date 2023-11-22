476425ed728c337e8bd3019ff9999267743e3840
Devinzenzhang <devinzenzhang@gmail.com>
2023-11-22 11:16:41 +0700

    fix: baca PDA dengan format yang diganti

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ else:
     file_PDA = sys.argv[1]
     file_HTML = sys.argv[2]
 
-PDA = PDAReader.read_PDA(file_PDA)
+states_set, input_alphabet, stack_alphabet, state, stack, final_states, Transition_Functions = PDAReader.read_PDA(file_PDA)
+remaining_input = [] # ganti dengan hasil parse HTML, misal ['a', 'b', 'dfvsdfvdfvadfv']
+
+while (len(remaining_input) > 0):
+    # proses
+    break
 
 # python3 main.py pda.txt
