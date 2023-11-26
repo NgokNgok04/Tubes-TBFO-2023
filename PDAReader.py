bc8bb0e514ba50c9097d8aea6837f0e2678c88b6
Devinzenzhang <devinzenzhang@gmail.com>
2023-11-26 12:47:25 +0700

    cleaning some stuff

--- PDAReader.py
+++ PDAReader.py
@@ -18,7 +18,21 @@ def read_PDA(PDA_Path):
         while (line != ""): # kosong artinya end of file
             if (line[0] == '#'): phase += 1
             line = split_komen(line).split()
-            if (phase == 1): # baca transition function
+            if (phase == 1): # baca states
+                states_set += line
+            elif (phase == 2): # baca input alphabet
+                input_alphabet += line
+            elif (phase == 3): # baca stack alphabet
+                stack_alphabet += line
+            elif (phase == 4): # baca start state
+                if (len(line) > 0):
+                    start_state = line[0]
+            elif (phase == 5): # baca start stack
+                if (len(line) > 0):
+                    start_stack = line[0].split('_')
+            elif (phase == 6): # baca final states
+                final_states += line
+            else: # baca transition functions
                 if (len(line) > 4):
                     Trans_Function = line[:4] + [line[4]]
                     Trans_Function[4] = Trans_Function[4].split('_')
@@ -26,38 +40,20 @@ def read_PDA(PDA_Path):
                         Trans_Function[4] = []
                     Trans_Function = tuple(Trans_Function)
                     Transition_Functions.append(Trans_Function)
-            elif (phase == 2): # baca input alphabet
-                input_alphabet += line
-            elif (phase == 3): # baca states
-                states_set += line
-            elif (phase == 4): # baca stack alphabet
-                stack_alphabet += line
-            elif (phase == 5): # baca start state
-                if (len(line) > 0):
-                    start_state = line[0]
-            elif (phase == 6): # baca start stack
-                if (len(line) > 0):
-                    start_stack = line[0].split('_')
-            else: # baca final states
-                final_states += line
             line = PDA_file.readline()
     return states_set, input_alphabet, stack_alphabet, start_state, start_stack, final_states, Transition_Functions
 
 """Format PDA
-##### State-Input-TOP-NextState-ContentStack ######
-State Input TOP NextState ContentStack # ganti
-State Input TOP NextState Content_Stack # push (dipisahkan dengan '_')
-State Input TOP NextState e # pop
-
-#####   Deskripsi input   #####
-a
-b
-c
 
 #####   Deskripsi state   #####
 A
 B
 C
+
+#####   Deskripsi input   #####
+a
+b
+c
 
 #### Deskripsi stack #####
 X
@@ -73,5 +69,10 @@ Z
 ##### Final States #####
 B
 C
+
+##### State-Input-TOP-NextState-ContentStack ######
+State Input TOP NextState ContentStack # ganti
+State Input TOP NextState Content_Stack # push (dipisahkan dengan '_')
+State Input TOP NextState e # pop
 
 """
